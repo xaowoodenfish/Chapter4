9c980acf9925aba4ef3ac9e0dc731f888c79a7c4
xaowoodenfish <153569267@qq.com>
2016-07-26 16:25:57 +0800

    Create insert_sort.py

---
+++ insert_sort.py
@@ -0,0 +1,15 @@
+import time
+def insert_sort(lists):
+  count = len(lists):
+  for i in range(1,count):
+    key = lists[i]
+    j = i-1
+    while j>=0:
+      if lists[j+1]>key:
+        lists[j+1] = lists[j]
+        lists[j] =  key
+      j -=1
+  return lists
+lists = [9,4,0,1,2,0,-5]
+lists = insert_sort(lists)
+print lists
