ae16494517fbfff55df026c0e8ad3e4de9f22e1c
xaowoodenfish <153569267@qq.com>
2016-07-26 17:47:46 +0800

    Create merge.py

---
+++ merge.py
@@ -0,0 +1,20 @@
+def merge(left,right):
+	i,j = 0,0
+	result = []
+	while i <len(left) and j <len(right):
+		if left[i]<=right[i]:
+			result.append(left[i]):
+			i += 1
+		else:
+			result.append(right[j])
+			j += 1
+		result += left[i:]
+		result += right[j:]
+	return result
+def merge_sort(lists):
+	if len(lists)<=1:
+		return lists
+	num = len(lists)/2
+	left = merge_sort(lists[:num])
+	right = merge_sort(lists[num:])
+	return merge(left,right)
